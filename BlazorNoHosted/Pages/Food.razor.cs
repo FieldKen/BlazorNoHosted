a84ae0e061c19b435e28e76a4ab3f26d9ebaa12c
Ken <fieldken@hotmail.com>
2024-04-05 00:16:54 +0200

    Hongo

---
+++ BlazorNoHosted/Pages/Food.razor.cs
@@ -0,0 +1,85 @@
+﻿using System.Web;
+
+namespace BlazorNoHosted.Pages
+{
+	public partial class Food
+	{
+		List<FoodQuestion> FoodQuestions { get; set; } = new List<FoodQuestion>();
+		public int CurrentQuestion { get; set; } = 1;
+
+		public List<HistoryItem> History { get; set; } = new List<HistoryItem>();
+
+		public Food()
+		{
+			FoodQuestions.Add(Question(0, "Are you hungry?", Answer("Yes", 8), Answer("No", 3)));
+			FoodQuestions.Add(Question(1, "Are you going out with friends?", Answer("Yes", 2), Answer("No", 0)));
+			FoodQuestions.Add(Question(2, "Your social life is important! Enjoy your meal!"));
+			FoodQuestions.Add(Question(3, "Do you really want to eat?", Answer("Yes", 4), Answer("No", 7)));
+			FoodQuestions.Add(Question(4, "REALLY, really?", Answer("Yes", 6), Answer("No", 7)));
+			FoodQuestions.Add(Question(5, "Go to the store and enjoy your meal!"));
+			FoodQuestions.Add(Question(6, "Do you have enough spare calories today, burned not included?", Answer("Yes", 10), Answer("No", 7)));
+			FoodQuestions.Add(Question(7, "Don't be a little bitch."));
+			FoodQuestions.Add(Question(8, "Really?", Answer("Yes", 6), Answer("No", 7)));
+			FoodQuestions.Add(Question(9, "Okay, whatever, enjoy your takeaway meal."));
+			FoodQuestions.Add(Question(10, "Would you be able to distract yourself for about hour, instead?", Answer("Yes", 7), Answer("No", 12)));
+			FoodQuestions.Add(Question(11, "Is there still time to go to the store?", Answer("Yes", 5), Answer("No", 9)));
+			FoodQuestions.Add(Question(12, "Do you have food at home?", Answer("Yes", 13), Answer("No", 14)));
+			FoodQuestions.Add(Question(13, "Enjoy your meal!"));
+			FoodQuestions.Add(Question(14, "Check again.", Answer("Yes", 13), Answer("No", 11)));
+		}
+
+		private FoodQuestion Question(int id, string question, params FoodAnswer[] answers)
+		{
+			return new FoodQuestion(id, question, answers.ToList());
+		}
+
+		private FoodAnswer Answer(string answer, int question)
+		{
+			return new FoodAnswer(answer, question);
+		}
+
+		public void NextQuestion(int id, string question, string answer)
+		{
+			History.Add(new HistoryItem(question, answer));
+			CurrentQuestion = id;
+		}
+	}
+
+	public class HistoryItem
+	{
+		public string Question { get; set; } = string.Empty;
+		public string Answer { get; set; } = string.Empty;
+
+		public HistoryItem(string question, string answer)
+		{
+			Question = question;
+			Answer = answer;
+		}
+	}
+
+	public class FoodQuestion
+	{
+		public int Id { get; set; } = -1;
+		public string Question { get; set; } = string.Empty;
+		public List<FoodAnswer> Answers { get; set; }
+
+		public FoodQuestion(int id, string question, List<FoodAnswer> answers)
+		{
+			Id = id;
+			Question = question;
+			Answers = answers;
+		}
+	}
+
+	public class FoodAnswer
+	{
+		public string Answer { get; set; } = string.Empty;
+		public int Question { get; set; } = -1;
+
+		public FoodAnswer(string answer, int question)
+		{
+			Answer = answer;
+			Question = question;
+		}
+	}
+}
--- BlazorNoHosted/Pages/Story.razor.cs
+++ BlazorNoHosted/Pages/Story.razor.cs
@@ -12,14 +12,14 @@ namespace BlazorNoHosted.Pages
 {
 	public partial class Story
 	{
-		public List<Question> Questions = new List<Question>();
+		public List<StoryQuestion> Questions = new List<StoryQuestion>();
 		public List<Person> People = new List<Person>();
 		public int Question { get; set; } = 1;
 		public List<int> Answers { get; set; } = new List<int>();
 
         public Story()
 		{
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"The morning light hadn't even begun to seep through the cracks when my eyes snapped open, confusion clouding my mind. Where was I? The room was bare, the walls cold and uninviting. Before I could even attempt to piece together how I ended up here, the door creaked open, and a shadowy figure stepped in. \"It's time,\" they said, their voice a chilling whisper that seemed to echo off the walls.",
 					"I had no idea what they meant, but their presence sent a shiver down my spine. They spoke of a choice I had to make, one that seemed straight out of a nightmare. Fight a tiger or an elephant, they said, in a battle where the stakes were life or death. And the catch? I'd be facing this beast with nothing but my bare hands.",
@@ -31,7 +31,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"Exhausted, every muscle in my body screaming, I stumbled out of the colosseum, the roars of the crowd fading behind me. The mysterious figure caught my attention, and offered me a reward as he held two small chests in his hand.",
 					"He explained, \"In one chest, there's a pill that will keep you strong and fit forever. The other chest has a pill that will protect you from ever getting sick or getting a serious disease when you get older.\"",
@@ -44,7 +44,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"With a silent nod, the mysterious figure handed me the chosen chest. In a surprising move, they crushed the pill from the other chest, leaving no trace of what could have been. Guiding me out of the colosseum, I took the pill, feeling its effects ripple through my body instantly, each cell buzzing with newfound vitality.",
 					"Stepping into the daylight, I found myself in a small, unfamiliar town, pockets empty, without a single coin to my name. As I wandered, searching for some way to get by, I came across an animal shelter. Desperate, I inquired about any available work. To my relief, they were in need of help.",
@@ -56,7 +56,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"With the money I earned from a day filled with furry companions, my stomach began to grumble, reminding me of the need to refuel. Walking into the heart of the town, I stumbled upon a quaint little restaurant, its menu curiously limited to just two items: pizza or burgers.",
 					"The waiter comes to me and asks if I had chosen what to eat. After a brief pause I confidently answer."
@@ -67,7 +67,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"As I made my choice between the pizza and burger, the waiter raised an eyebrow, seemingly surprised by my decision, but jotted it down nonetheless. He turned to leave, then paused, spinning back around with a curious glance. \"And what can I get you to drink?\" he asked.",
 					"I pondered for a moment, caught between the desire for something alcoholic to unwind after the day's unexpected trials and adventures, or opting for a non-alcoholic beverage to stay sharp and refreshed."
@@ -78,7 +78,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"After enjoying my meal, I realized I needed to figure out where to sleep. My money was running low, and there were two hotels nearby that caught my eye. One was clearly budget-friendly, nothing fancy, but it looked clean enough. The other hotel was way out of my price range, luxurious and inviting, promising a really comfortable night's sleep.",
 					"I was stuck deciding between saving my money and staying somewhere simple, or treating myself to a good night's rest after such a long day but risking running out of money. It was a tough call, especially since I didn't know how long I'd be in this town or what expenses might come up next."
@@ -88,7 +88,7 @@ namespace BlazorNoHosted.Pages
 					"I want to be treated well and have a comfortable night of sleep."
 				])
 			);
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"Lying in bed, the quiet of the room was suddenly broken by the sound of music drifting in through the window. Intrigued, I decided to slip out and see where it was coming from. The lively beats led me to a street party, where the energy was palpable, people moving to the rhythm, completely lost in the moment.",
 					"As I stood there, taking it all in, some party-goers noticed me and gestured for me to join them in the dance.I found myself at a crossroads, unsure whether to dive into the festivities or remain a spectator on the sidelines."
@@ -99,7 +99,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"DJ Qetor, perched behind his setup, caught my eye and gestured me over. Curiosity getting the better of me, I approached, and we struck up a conversation about music and the vibrant energy of the night. He was surprisingly easy to talk to, and it felt like we'd known each other for more than just a few minutes.",
 					"After a while, he handed me a pair of headphones and asked, \"What song would you like to hear next?\" It was an unexpected offer, one that felt like a small but significant way to leave my mark on the evening."
@@ -110,7 +110,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"As the first light of dawn began to paint the sky, the party's energy showed no sign of waning. Yet, in that serene moment of transition from night to day, the mysterious figure reappeared, as if out of nowhere, their presence instantly commanding my attention amidst the fading beats.",
 					"They leaned in, their voice barely above the music, and said, \"You only have time for one more adventure here. What will it be? Do you wish to explore the hidden gems of the city, or would you prefer to immerse yourself in the natural beauty that surrounds it?\"",
@@ -122,7 +122,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"I realized it was time to think about heading back home. But how should I get there? I could hop on a plane and be back before I knew it, or I could turn the journey into an adventure of its own with a road trip.",
 					"The thought of flying was tempting for its speed and simplicity, but the idea of driving back, taking in new sights along the way, also had its appeal. It was a tough choice. A plane ride meant getting back to routine quickly, while a road trip could extend this unexpected escape a little longer, offering a chance to reflect on everything that had happened."
@@ -133,7 +133,7 @@ namespace BlazorNoHosted.Pages
 				])
 			);
 
-			Questions.Add(new Question(
+			Questions.Add(new StoryQuestion(
 				[
 					"Arriving back home, I paused to consider how I wanted to spend the rest of my day. The thought of playing video games and catching up on TV shows was appealing, a way to relax and slowly ease back into my regular life. It felt like a cozy option, just unwinding on the couch with some snacks and letting myself zone out for a bit.",
 					"On the other hand, getting outside for some sports sounded energizing. It could be a great way to keep the momentum of my recent adventures going, using the exercise to clear my mind and maybe reflect on everything that had happened."
@@ -230,12 +230,12 @@ namespace BlazorNoHosted.Pages
 			return result;
 		}
 	}
-	public class Question
+	public class StoryQuestion
 	{
 		public List<string> FullStory = new List<string>();
 		public List<string> Answers { get; set; }
 
-		public Question(List<string> fullStory, List<string> answers)
+		public StoryQuestion(List<string> fullStory, List<string> answers)
 		{
 			FullStory = fullStory;
 			Answers = answers;
